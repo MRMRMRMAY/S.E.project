02a9d08833735a4156844080399b6be6ea745263
jjh <burgerstealer@gmail.com>
2016-06-10 19:38:39 +0900

    Revert "2016.5.29"
    This reverts commit baee28557fa012c13a03a01c50ad83b81c3d0251.

--- meihaoran/mytest/src/controller/moder/MapPanel.java
+++ meihaoran/mytest/src/controller/moder/MapPanel.java
@@ -9,232 +9,62 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import controller.MainApp;
 public class MapPanel extends JPanel{
-	private MainApp mainApp = new MainApp();
 	private int c = 0;
 	private JFrame parentWindow;
-	private JButton addButton ;
+	private JButton add ;
 	private JButton delet;
 	private JButton edit ;
-	private JTextField xInput;
-	private JTextField yInput;
-	private JTextField nameInput;
-	private JTextField toNumberInput;
-	private JTextField distanceInput;
-	private JLabel nameLabel;
-	private JLabel xLabel;
-	private JLabel yLabel;
-	private JLabel toNumberLabel;
-	private JLabel distanceLabel;
-	MapStart main;
-	static ArrayList<Spot> SpotList = new ArrayList<Spot>();
-	static ArrayList<Distance> distanceList = new ArrayList<Distance>();
-	static Map map = new Map();
+	private JTextField selet;
 	public MapPanel(JFrame window){
 		parentWindow = window;
 		init();
 	}
 	public void init(){
-		addButton = new JButton();
-		delet = new JButton();
-		edit = new JButton();
-		xInput = new JTextField(5);
-		yInput = new JTextField(5);
-		xLabel = new JLabel();
-		yLabel = new JLabel();
-		nameInput = new JTextField(5);
-		nameLabel = new JLabel();
-		toNumberLabel = new JLabel();
-		toNumberInput = new JTextField(5);
-		distanceLabel = new JLabel();
-		distanceInput = new JTextField(5);
-		addButton.setText("Add");
-		addButton.setHorizontalAlignment(addButton.CENTER);
-		addButton.setVerticalAlignment(addButton.CENTER);
-		addButton.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){
-				int x = 0;
-				int y = 0;
-				int distance = 0;
-				int to = 0;
-				boolean exit = false;
-				Spot spot = new Spot();
-				
-				
-				System.out.println(0);
-				try{
-					x = Integer.parseInt(xInput.getText());
-					y = Integer.parseInt(yInput.getText());
-					to = Integer.parseInt(toNumberInput.getText());
-				}
-				catch(NumberFormatException exception){
-					exception.printStackTrace();
-				}
-				if(!SpotList.isEmpty()){
-					for(Spot item: SpotList){
-						if(item.getSpotName().equals(nameInput.getText())){
-							exit = true;
-							break;
-						}
-					}
-				}
-				if(!exit){
-					spot.setSpotName(nameInput.getText());
-					spot.setSpotIndex(SpotList.size());
-					SpotList.add(spot);
-					Distance distanceE = new Distance();
-					distanceE.addSpot(SpotList.get(to), distance);
-					distanceList.add(spot.getSpotIndex(), distanceE);
-					if(x>=0&&x<=400&&y>=0&&y<=700){
-						main.addPoint(x,y,spot);
-					}
-					else{
-						mainApp.showDialog("Input 0<=x<=400 and 0<=y<=700","InputError" );
-					}
-					
-				}
-				else{
-					mainApp.showDialog("spot exited","spot exited" );
-				}
-			}
-			});
-		this.add(addButton);
+		JButton add = new JButton();
+		JButton delet = new JButton();
+		JButton edit = new JButton();
+		add.setText("Add");
+		add.setHorizontalAlignment(add.CENTER);
+		add.setVerticalAlignment(add.CENTER);
+		this.add(add);
 		delet.setText("Del");
 		delet.setHorizontalAlignment(delet.CENTER);
 		delet.setVerticalAlignment(delet.CENTER);
-		delet.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){	
-				System.out.println(0);
-				boolean exit = false;
-				Spot spot = new Spot();
-				
-				if(!SpotList.isEmpty()){
-					for(Spot item: SpotList){
-						if(item.getSpotName().equals(nameInput.getText())){
-							exit = true;
-							spot = item;
-							main.deletPoint(spot);
-							SpotList.remove(item);
-						}
-					}
-				}
-				
-			}
-			});
 		this.add(delet);
 		edit.setText("Edit");
 		edit.setHorizontalAlignment(edit.CENTER);
 		edit.setVerticalAlignment(edit.CENTER);
-		edit.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e){	
-				int distance = 0;
-				int to = 0;
-				System.out.println(0);
-				boolean exit = false;
-				Spot spot = new Spot();
-				try{
-					to = Integer.parseInt(toNumberInput.getText());
-				}
-				catch(NumberFormatException exception){
-					exception.printStackTrace();
-				}
-				
-				if(!SpotList.isEmpty()){
-					for(Spot item: SpotList){
-						if(item.getSpotName().equals(nameInput.getText())){
-							exit = true;
-							spot = item;
-							break;
-						}
-					}
-				}
-				if(exit){
-					int index = spot.getSpotIndex();
-					Distance distanceE = distanceList.get(index);
-					Spot toSpot = SpotList.get(to);
-					distanceE.getDistance().replace(toSpot, distance);
-					distanceList.set(index, distanceE);
-				}				
-			}
-			});
 		this.add(edit);
-		xLabel.setText("x : ");
-		xLabel.setHorizontalAlignment(xLabel.CENTER);
-		xLabel.setVerticalAlignment(xLabel.CENTER);
-		this.add(xLabel);
-		xInput.setHorizontalAlignment(xInput.CENTER);
-		this.add(xInput);
-		yLabel.setText("y : ");
-		yLabel.setHorizontalAlignment(yLabel.CENTER);
-		yLabel.setVerticalAlignment(yLabel.CENTER);
-		this.add(yLabel);
-		xInput.setHorizontalAlignment(yInput.CENTER);
-		this.add(yInput);
-		nameLabel.setText("name : ");
-		nameLabel.setHorizontalAlignment(nameLabel.CENTER);
-		nameLabel.setVerticalAlignment(nameLabel.CENTER);
-		this.add(nameLabel);
-		nameInput.setHorizontalAlignment(nameInput.CENTER);
-		this.add(nameInput);
-		
-		toNumberLabel.setText("to : ");
-		toNumberLabel.setHorizontalAlignment(toNumberLabel.CENTER);
-		toNumberLabel.setVerticalAlignment(toNumberLabel.CENTER);
-		this.add(toNumberLabel);
-		toNumberInput.setHorizontalAlignment(toNumberInput.CENTER);
-		this.add(toNumberInput);
-		
-		distanceLabel.setText("distanceInput : ");
-		distanceLabel.setHorizontalAlignment(distanceLabel.CENTER);
-		distanceLabel.setVerticalAlignment(distanceLabel.CENTER);
-		this.add(distanceLabel);
-		distanceInput.setHorizontalAlignment(distanceInput.CENTER);
-		this.add(distanceInput);
+		selet = new JTextField(10);
+		selet.setText("No.place");
+		this.add(selet);
 		
 	}
-	
 	public Dimension getMinimumSize(){
 		return getPreferredSize();
 	}
 	public Dimension getPreferredSize(){
-		return new Dimension(800,400);
-	}
-	public void addComponent(JLabel item){
-		this.add(item);
-	}
-	public void setMainFrame(MapStart map){
-		this.main = map;
-	}
-	public void deletComponent(JLabel item){
-		this.remove(item);
+		return new Dimension(600,300);
 	}
 	public void paintComponent(Graphics g){
-//		super.paintComponent(g);
-//		Graphics2D g2 = (Graphics2D) g;
-//		MapData data = new MapData();
-//		if(!data.point.isEmpty())
-//		for(Point item:data.point){
-//			g.setColor(Color.blue);
-//			g.fillOval(item.getX()+100,item.getY()+100,10,10);
-//			if(!item.getTo().isEmpty()){
-//				for(int toNum: item.getTo()){
-//					Line2D line = new Line2D.Float((float)(item.getX()+100),(float)(item.getY()+100),
-//							(float)(data.point.get(toNum-1).getX()+100),(float)(data.point.get(toNum-1).getY()+100));
-//					g2.draw(line);
-//				}
-//			}
-//		}
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D) g;
+		MapData data = new MapData();
+		if(!data.point.isEmpty())
+		for(Point item:data.point){
+			g.setColor(Color.blue);
+			g.fillOval(item.getX(),item.getY(),10,10);
+			if(!item.getTo().isEmpty()){
+				for(int toNum: item.getTo()){
+					Line2D line = new Line2D.Float((float)(item.getX()),(float)(item.getY()),
+							(float)(data.point.get(toNum-1).getX()),(float)(data.point.get(toNum-1).getY()));
+					g2.draw(line);
+				}
+			}
+		}
 //		for(int i = 0;i<3;i++){
 //			g.setColor(Color.blue);
 //			g.fillOval(i*30, i*30, 20, 20);
@@ -244,5 +74,4 @@ public class MapPanel extends JPanel{
 //			g2.draw(lin);
 //		}
 	}
-	
 }
--- meihaoran/mytest/src/controller/moder/MapStart.java
+++ meihaoran/mytest/src/controller/moder/MapStart.java
@@ -1,39 +1,12 @@
 package controller.moder;
-import java.util.HashMap;
-
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 public class MapStart extends JFrame{
-	private static MapPanel mapPanle;
-	private static HashMap<Spot, JLabel>point = new HashMap<Spot,JLabel>();
-	private static boolean flag = true;
-	private static JFrame frame;
+	private final MapPanel mapPanle;
 	public MapStart(){
-		frame = new JFrame();
-		mapPanle = new MapPanel(frame);
-		frame.add(mapPanle);
-		mapPanle.setMainFrame(this);
-		frame.pack();
-		frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		frame.setResizable(false);
-		frame.setVisible(true);
+		mapPanle = new MapPanel(this);
+		add(mapPanle);
+		pack();
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
 	}
-	public static void addPoint(int x, int y, Spot name){
-		JLabel item = new JLabel();
-		item.setIcon(new ImageIcon("D:/SE/project/mytest/src/controller/moder/Point.png"));
-		item.setBounds(x, y,100, 100);
-		item.setText(name.getSpotName());
-		point.put(name, item);
-		frame.add(item);
-		frame.repaint();
-	}
-	public void deletPoint(Spot name){
-		if(flag){
-			JLabel item = point.get(name);
-			frame.remove(item);
-			frame.repaint();
-		}
-	}
-	
 }
